4b002e55114fe2564890dcf824f7cf2f12b01c08
bn-tw2020 <skaxodn97@gmail.com>
2022-07-24 15:59:42 +0900

    chore: Gradle.kts 버전 관리
    - Gradle Groovy → Gradle Kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = AppConfig.compileSdk
+
+    defaultConfig {
+        applicationId = AppConfig.applicationId
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+        versionCode = AppConfig.versionCode
+        versionName = AppConfig.versionName
+
+        testInstrumentationRunner = AppConfig.testInstrumentationRunner
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        dataBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(Library.AndroidX.CORE_KTX)
+    implementation(Library.AndroidX.APPCOMPAT)
+    implementation(Library.AndroidX.MATERIAL)
+    implementation(Library.AndroidX.CONSTRAINT_LAYOUT)
+    testImplementation(Library.Junit.JUNIT)
+    androidTestImplementation(Library.AndroidX.TEST_JUNIT)
+    androidTestImplementation(Library.AndroidX.ESPRESSO)
+}
---
+++ buildSrc/src/main/java/AppConfig.kt
@@ -0,0 +1,9 @@
+object AppConfig {
+    const val compileSdk = 32
+    const val applicationId = "com.dnd.niceteam"
+    const val minSdk = 21
+    const val targetSdk = 32
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
