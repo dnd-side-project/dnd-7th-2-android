76b9276694f17f369a38737ebfa9bf0b25e52e8c
sjunh812 <sjunh812@naver.com>
2022-08-05 17:58:58 +0900

    style: 코드 정렬

--- app/src/main/java/com/dnd/niceteam/base/BaseActivity.kt
+++ app/src/main/java/com/dnd/niceteam/base/BaseActivity.kt
@@ -6,7 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
-abstract class BaseActivity<T: ViewDataBinding>(@LayoutRes val layoutRes: Int) : AppCompatActivity(layoutRes) {
+abstract class BaseActivity<T : ViewDataBinding>(@LayoutRes val layoutRes: Int) :
+    AppCompatActivity(layoutRes) {
     lateinit var binding: T
 
     override fun onCreate(savedInstanceState: Bundle?) {
