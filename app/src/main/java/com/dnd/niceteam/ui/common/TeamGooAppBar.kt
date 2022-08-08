3f161986c6529eecca27cb8a9c5b1ad04952e019
bn-tw2020 <skaxodn97@gmail.com>
2022-08-08 20:57:23 +0900

    feat: 커스텀 앱바 수정
    - 커스텀 앱바 action Icon 최소 0개 ~ 최대 2개 등록가능

--- app/src/main/java/com/dnd/niceteam/ui/common/TeamGooAppBar.kt
+++ app/src/main/java/com/dnd/niceteam/ui/common/TeamGooAppBar.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import com.dnd.niceteam.R
 import com.dnd.niceteam.databinding.ViewAppbarBinding
@@ -15,7 +17,8 @@ class TeamGooAppBar(context: Context, attrs: AttributeSet) : ConstraintLayout(co
 
     private val title = binding.tvTitle
     private val navigationIcon = binding.ivNavigationIcon
-    private val actionIcon = binding.ivActionIcon
+    private val firstAction = binding.ivFirstAction
+    private val secondAction = binding.ivSecondAction
 
     init {
         initAttrs(attrs)
@@ -29,25 +32,31 @@ class TeamGooAppBar(context: Context, attrs: AttributeSet) : ConstraintLayout(co
             0
         ).apply {
             try {
-                setTitle(getResourceId(R.styleable.TeamGooAppBar_title, 0))
+                setTitle(getString(R.styleable.TeamGooAppBar_title))
                 setNavigationIcon(getResourceId(R.styleable.TeamGooAppBar_icon_navigation, 0))
-                setActionIcon(getResourceId(R.styleable.TeamGooAppBar_icon_navigation, 0))
+                setFirstAction(getResourceId(R.styleable.TeamGooAppBar_icon_first_action, 0))
+                setSecondAction(getResourceId(R.styleable.TeamGooAppBar_icon_second_action, 0))
             } finally {
                 recycle()
             }
         }
     }
 
-
-    private fun setTitle(resourceId: Int) {
-        if (resourceId != 0) title.setText(resourceId)
+    private fun setTitle(title: String?) {
+        if (title != null) this.title.text = title
     }
 
     private fun setNavigationIcon(resourceId: Int) {
         if (resourceId != 0) navigationIcon.setImageResource(resourceId)
     }
 
-    private fun setActionIcon(resourceId: Int) {
-        if (resourceId != 0) actionIcon.setImageResource(resourceId)
+    private fun setFirstAction(resourceId: Int) {
+        if (resourceId != 0) firstAction.setImageResource(resourceId)
+        else firstAction.isVisible = isGone
+    }
+
+    private fun setSecondAction(resourceId: Int) {
+        if (resourceId != 0) secondAction.setImageResource(resourceId)
+        else secondAction.isVisible = isGone
     }
 }
