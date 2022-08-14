76b01ea3d06ee66c7e30a0cd93587f6cc7fe0d69
bn-tw2020 <skaxodn97@gmail.com>
2022-08-14 16:44:01 +0900

    feat: 로그인 뷰모델 생성

--- app/src/main/java/com/dnd/niceteam/ui/onboarding/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/dnd/niceteam/ui/onboarding/login/viewmodel/LoginViewModel.kt
@@ -1,8 +1,13 @@
 package com.dnd.niceteam.ui.onboarding.login.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dnd.niceteam.ui.onboarding.Validation
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,18 +16,21 @@ class LoginViewModel @Inject constructor() : ViewModel() {
     var email = MutableStateFlow<String>("")
     var password = MutableStateFlow<String>("")
 
-    fun validateEmail(handleInputSuccess: () -> Unit, handleInputError: () -> Unit) {
-        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
-        return when {
-            email.value.isEmpty() -> {
-                handleInputSuccess()
-            }
-            !email.value.matches(emailPattern.toRegex()) -> {
-                handleInputError()
-            }
-            else -> {
-                handleInputSuccess()
-            }
+    private val _signUpEvent = MutableSharedFlow<Unit>()
+    val signUpEvent = _signUpEvent.asSharedFlow()
+
+    fun validateEmail(
+        handleInputSuccess: () -> Unit,
+        handleInputError: () -> Unit
+    ) {
+        when (Validation.validateEmail(email.value)) {
+            null -> handleInputSuccess()
+            false -> handleInputError()
+            true -> handleInputSuccess()
         }
+    }
+
+    fun navigateToSignUp() = viewModelScope.launch {
+        _signUpEvent.emit(Unit)
     }
 }
